929f852cb9c1be2be6c2747c0103165d993872b1
Shazil Ahmed <shazilahmed445@gmail.com>
2023-08-01 07:17:09 -0400

    intial commit

--- web.py
+++ web.py
@@ -22,4 +22,4 @@ for index,todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="", placeholder="Enter todo", on_change=add_todo, key="new_todo")     
+st.text_input(label="Enter your input here", placeholder="Enter todo", on_change=add_todo, key="new_todo")     
